b99d5d5aeb534e609e266befdfeb9e0d68d51e11
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-10 22:17:27 +0530

    Create increment and decrement count

---
+++ redux-01/src/features/counter/Counter.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { increment, decrement } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
