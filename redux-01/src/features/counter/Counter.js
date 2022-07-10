778d482e5e4dc7e6a78ab20f5af41060ebb479f3
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-10 22:39:43 +0530

    Create Add Amount and reset amount payload function

--- redux-01/src/features/counter/Counter.js
+++ redux-01/src/features/counter/Counter.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+
+import { useState } from "react";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+
+
+    const [incrementAmount, setIncrementAmount] = useState(0);
+
+    const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
 
   return (
     <section>
@@ -13,6 +25,18 @@ const Counter = () => {
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
       </div>
     </section>
   );
