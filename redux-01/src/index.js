a2dcd20736db710e8e0afa7ec5f035bbf1171190
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-10 22:18:42 +0530

    import Provider component inside in to App.js file

--- redux-01/src/index.js
+++ redux-01/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 import "./index.css";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDom.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.querySelector("#root")
 );
